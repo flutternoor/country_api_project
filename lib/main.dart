126660b745b35f2b2539f16f79d2fb03ab4b891a
noor <noor.ptcp@gmail.com>
2022-10-30 23:51:57 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:country_api_project/screens/country_list_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Country Project',
+      theme: ThemeData(
+
+        primarySwatch: Colors.brown,
+      ),
+      home: const CountryListScreen(),
+    );
+  }
+}
+
+
